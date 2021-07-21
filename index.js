e8003f524043fd3db64692b54a263896aa3a0f99
Harsh-Khurana <harshkhharshkhurana@gmail.com>
2021-07-21 08:03:47 +0530

    restructuring, styling auth, initial products setup

--- index.js
+++ index.js
@@ -1,75 +1,17 @@
 const express = require('express');
 const cookieSession = require('cookie-session');
-const userRepo = require('./repositories/users');
+const authRouter = require('./routes/admin/auth');
+const productRouter = require('./routes/admin/products');
 
 const app = express();
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended : true }));
 app.use(cookieSession({
     keys : ['mycookiesecret']
 }))
-
-app.get('/signup', (req, res)=>{
-    res.send(`
-        <div>
-            Your id :${req.session.userId}<br>
-            <form method="POST" action="signup">
-                <input name="email" placeholder="email"/>
-                <input name="password" placeholder="password"/>
-                <input name="passwordConfirmation" placeholder="password confirmation"/>
-                <button>Sign up</button>
-            </form>
-        </div>
-    `);
-})
-
-app.post('/signup', async (req, res)=>{
-    const { email, password, passwordConfirmation } = req.body;
-    const existingUser = await userRepo.getOneBy({ email });
-
-    if(existingUser){
-        return res.status(400).send('Email in use');
-    }
-    if(password !== passwordConfirmation){
-        return res.status(400).send('Passwords dont match');
-    }
-    const user = await userRepo.create({ email, password });
-    req.session.userId = user.id;
-    res.send('Account created!!');
-})
-
-app.get('/signout', (req, res)=>{
-    req.session = null;
-    res.send('You are logged out');
-})
-
-app.get('/signin', (req, res)=>{
-    res.send(`
-        <div>
-            <form method="POST" action="/signin">
-                <input name="email" placeholder="email"/>
-                <input name="password" placeholder="password"/>
-                <button>Sign In</button>
-            </form>
-        </div>
-    `);
-})
-
-app.post('/signin', async(req, res)=>{
-    const { email, password } = req.body;
-    const user = await userRepo.getOneBy({ email });
-
-    if(!user){
-        return res.status(400).send('Email not found');
-    }
-
-    const validPassword = await userRepo.comparePassword(user.password, password);
-    if(!validPassword){
-        return res.status(400).send('Invalid password');
-    }
-    req.session.userId = user.id;
-    res.send('You are signed in');
-})
+app.use(authRouter);
+app.use(productRouter);
 
 app.listen(3000, ()=>{
     console.log('Server started');
---
+++ routes/admin/validators.js
@@ -0,0 +1,59 @@
+const { check } = require('express-validator');
+const usersRepo = require('../../repositories/users');
+
+module.exports = {
+    requireTitle : check('title')
+        .trim()
+        .isLength({ min : 5, max : 30 }),
+    requirePrice : check('price')
+        .trim()
+        .toFloat()
+        .isFloat({ min : 1 }),
+    requireEmail :  check('email')
+        .trim()
+        .normalizeEmail()
+        .isEmail()
+        .withMessage('Must provide a valid email')
+        .custom( async email=>{
+            const existingUser = await usersRepo.getOneBy({ email });
+            if(existingUser){
+                throw new Error('Email in use');
+            }
+        }),
+    requirePassword : check('password')
+        .trim()
+        .isLength({ min : 4, max : 20 })
+        .withMessage('Password length should be between 4 and 20 characters'),
+    requirePassswordConfirmation : check('passwordConfirmation')
+        .trim()
+        .isLength({ min : 4, max : 20 })
+        .withMessage('Password length should be between 4 and 20 characters')
+        .custom(async (passwordConfirmation, {req})=>{
+            if(req.body.password !== passwordConfirmation){
+                throw new Error('Passwords must match');
+            }
+        }),
+    requireEmailExists : check('email')
+        .trim()
+        .normalizeEmail()
+        .isEmail()
+        .withMessage('Must provide a valid email')
+        .custom(async email=>{
+            const user = await usersRepo.getOneBy({ email });
+            if(!user){
+                throw new Error('Email not found');
+            }
+        }),
+    requireValidPasswordForUser : check('password')
+        .trim()
+        .custom(async (password, {req})=>{
+            const user = await usersRepo.getOneBy({ email : req.body.email });
+            if(!user){
+                throw new Error('Invalid password');
+            }
+            const validPassword = await usersRepo.comparePassword(user.password, password);
+            if(!validPassword){
+                throw new Error('Invalid password');
+            }
+        })
+}
