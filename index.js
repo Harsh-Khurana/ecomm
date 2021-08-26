c5ccf222acd8eac47e7c5c15b8fec90d25dc3dff
Harsh-Khurana <harshkhharshkhurana@gmail.com>
2021-08-26 07:03:10 +0530

    production ready

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
 const adminProductRouter = require('./routes/admin/products');
@@ -6,17 +7,18 @@ const productRouter = require('./routes/products');
 const cartRouter = require('./routes/carts');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended : true }));
 app.use(cookieSession({
-    keys : ['mycookiesecret']
+    keys : [process.env.COOKIE_SECRET]
 }))
 app.use(authRouter);
 app.use(adminProductRouter);
 app.use(productRouter);
 app.use(cartRouter);
 
-app.listen(3000, ()=>{
-    console.log('Server started');
+app.listen(PORT, ()=>{
+    console.log(`Server running at ${PORT}`);
 })
